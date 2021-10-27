67a1ae7620311392d1d4e6460ad189ed5cb047b0
phongz <phongnguyenzzz991@gmai.com>
2021-10-27 13:31:14 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,28 @@
+package basic.dev;
+
+import java.util.Scanner;
+
+public class MainApp {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("nhap vao ten ban");
+		String line = sc.nextLine();
+		System.out.println(line);
+		System.out.println("nhap vao so nguyen");
+		int i = sc.nextInt();
+		System.out.println(i);
+		System.out.println("nhap vao so nguyen long");
+		long l = sc.nextLong();
+		System.out.println(l);
+		System.out.println("nhap vao so thuc float");
+		float f = sc.nextFloat();
+		System.out.println(f);
+		System.out.println("nhap vao so thuc double");
+		double d = sc.nextDouble();
+		System.out.println(d);
+	}	
+	
+	} 
+	
+
+
